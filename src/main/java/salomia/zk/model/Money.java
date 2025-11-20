0fd377c5de584ea775c506022521a479a67d8859
SalomiaZK <salomia.razakarivony@gmail.com>
2025-11-20 19:54:15 +0300

    feat: CI + Lint

--- src/main/java/salomia/zk/model/Money.java
+++ src/main/java/salomia/zk/model/Money.java
@@ -1,4 +1,9 @@
 package salomia.zk.model;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Money {
+    private final String currency;
+    private final String amount;
 }
--- src/main/java/salomia/zk/model/Wallet.java
+++ src/main/java/salomia/zk/model/Wallet.java
@@ -1,4 +1,29 @@
 package salomia.zk.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
 public class Wallet {
+    private final  String colour;
+    private final String size;
+    private final ArrayList<Money> money;
+
+    public ArrayList<Money> checkVola(Money money) {
+        return this.money.stream().filter(money1 -> money1.equals(money) ).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public ArrayList<Money> checkVola() {
+        return getMoney();
+    }
+
+    public ArrayList<Money> addVola(ArrayList<Money> money) {
+        this.money.addAll(money);
+        return  this.money;
+    }
 }
